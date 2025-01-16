ab47aa28802712f960e91868c36131bd2318617c
Andrea Gamble <abgamble00@gmail.com>
2025-01-16 14:40:48 -0500

    GUI draft1

---
+++ GUI/username-password.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('Login')
+root.geometry('400x100+150+150')
+root.resizable(False,False)
+
+usernameField = ttk.Entry(root, width = 30)
+usernameField.pack()
+usernameField.insert(0,'Username')
+
+passwordField = ttk.Entry(root, width = 30)
+passwordField.pack()
+passwordField.insert(0,'Password')
+
+loginButton = ttk.Button(root, text = 'Login')
+loginButton.pack()
+
+def mainFrame():
+    mainFrame = Toplevel(root)
+
+loginButton.config(command = mainFrame)
