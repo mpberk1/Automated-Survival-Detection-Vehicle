ada0f9cfa5a3dd09f008aafc00004b4c2b8d2410
mpberk1 <berkowim@my.erau.edu>
2025-01-30 14:54:03 -0500

    Pygame sound

--- Sound/soundDriver.py
+++ Sound/soundDriver.py
@@ -1,8 +1,12 @@
-from pydub import AudioSegment
-from pydub.playback import play
+import pygame
 
-# Load the MP3 file
-audio = AudioSegment.from_mp3("LifeCheck.mp3")
+# Initialize pygame mixer
+pygame.mixer.init()
 
-# Play the audio
-play(audio)
+# Load and play the MP3 file
+pygame.mixer.music.load("/home/seed/490/Automated-Survival-Detection-Vehicle/Sound/LifeCheck.mp3")
+pygame.mixer.music.play()
+
+# Keep the script running while the music plays
+while pygame.mixer.music.get_busy():
+    continue
