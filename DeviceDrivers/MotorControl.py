18b7265f881fcc1e7c65d612c39503800c9a7546
mpberk1 <berkowim@my.erau.edu>
2025-02-20 14:49:08 -0500

    More motro stuff

--- DeviceDrivers/MotorControl.py
+++ DeviceDrivers/MotorControl.py
@@ -1,82 +1,64 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO pins for Motor 1
-M1_IN1 = 17  # Motor 1 Forward
-M1_IN2 = 27  # Motor 1 Reverse
-
-# Define GPIO pins for Motor 2
-M2_IN1 = 22  # Motor 2 Forward
-M2_IN2 = 23  # Motor 2 Reverse
+# Assign a single GPIO pin per motor
+MOTOR1_CTRL = 17  # Motor 1 control
+MOTOR2_CTRL = 22  # Motor 2 control
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup([M1_IN1, M1_IN2, M2_IN1, M2_IN2], GPIO.OUT)
+GPIO.setup(MOTOR1_CTRL, GPIO.OUT)
+GPIO.setup(MOTOR2_CTRL, GPIO.OUT)
 
 def forward():
-    print("Moving Forward")
-    GPIO.output(M1_IN1, GPIO.HIGH)
-    GPIO.output(M1_IN2, GPIO.LOW)
-    GPIO.output(M2_IN1, GPIO.HIGH)
-    GPIO.output(M2_IN2, GPIO.LOW)
+    print("Moving forward")
+    GPIO.output(MOTOR1_CTRL, GPIO.HIGH)
+    GPIO.output(MOTOR2_CTRL, GPIO.HIGH)
 
 def backward():
-    print("Moving Backward")
-    GPIO.output(M1_IN1, GPIO.LOW)
-    GPIO.output(M1_IN2, GPIO.HIGH)
-    GPIO.output(M2_IN1, GPIO.LOW)
-    GPIO.output(M2_IN2, GPIO.HIGH)
-
-def left():
-    print("Turning Left")
-    GPIO.output(M1_IN1, GPIO.LOW)  # Stop M1
-    GPIO.output(M1_IN2, GPIO.LOW)
-    GPIO.output(M2_IN1, GPIO.HIGH) # Move M2 Forward
-    GPIO.output(M2_IN2, GPIO.LOW)
-
-def right():
-    print("Turning Right")
-    GPIO.output(M1_IN1, GPIO.HIGH) # Move M1 Forward
-    GPIO.output(M1_IN2, GPIO.LOW)
-    GPIO.output(M2_IN1, GPIO.LOW)  # Stop M2
-    GPIO.output(M2_IN2, GPIO.LOW)
+    print("Moving backward")
+    GPIO.output(MOTOR1_CTRL, GPIO.LOW)
+    GPIO.output(MOTOR2_CTRL, GPIO.LOW)
 
 def stop():
     print("Stopping")
-    GPIO.output(M1_IN1, GPIO.LOW)
-    GPIO.output(M1_IN2, GPIO.LOW)
-    GPIO.output(M2_IN1, GPIO.LOW)
-    GPIO.output(M2_IN2, GPIO.LOW)
+    GPIO.output(MOTOR1_CTRL, GPIO.LOW)
+    GPIO.output(MOTOR2_CTRL, GPIO.LOW)
+
+def left():
+    print("Turning left")
+    GPIO.output(MOTOR1_CTRL, GPIO.LOW)  # One motor stops or reverses
+    GPIO.output(MOTOR2_CTRL, GPIO.HIGH)
+
+def right():
+    print("Turning right")
+    GPIO.output(MOTOR1_CTRL, GPIO.HIGH)
+    GPIO.output(MOTOR2_CTRL, GPIO.LOW)
 
 def motor_control():
     try:
         while True:
             forward()
+            print("Moving forward for 2 seconds")
             time.sleep(2)
+
             stop()
             time.sleep(1)
 
             backward()
+            print("Moving backward for 2 seconds")
             time.sleep(2)
-            stop()
-            time.sleep(1)
 
-            left()
-            time.sleep(1)
             stop()
-            time.sleep(1)
-
-            right()
-            time.sleep(1)
-            stop()
-            time.sleep(1)
+            print("Waiting 5 seconds")
+            time.sleep(5)
 
     except KeyboardInterrupt:
         print("Program interrupted by user")
-    
+
     finally:
-        stop()  # Ensure motors are stopped
-        GPIO.cleanup()  # Reset GPIO pins
+        stop()
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     motor_control()
